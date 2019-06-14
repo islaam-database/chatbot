5de09f0698229c6156a89c2b17b68cbfabacb4a1
Yousef Shanawany <askyous@gmail.com>
2019-06-13 21:23:30 -0700

    test df response

--- fulfillment.cs
+++ fulfillment.cs
@@ -7,10 +7,11 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Google.Apis.Dialogflow.v2.Data;
 
 namespace IslaamDatabase
 {
-    public static class fulfillment
+    public static class Fulfillment
     {
         [FunctionName("fulfillment")]
         public static async Task<IActionResult> Run(
@@ -19,14 +20,12 @@ namespace IslaamDatabase
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            var data = JsonConvert.DeserializeObject<GoogleCloudDialogflowV2WebhookRequest>(requestBody);
+            var response = new GoogleCloudDialogflowV2WebhookResponse { FulfillmentText = "Hello yousef." };
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"GIT Hello, {name}")
+            return data != null
+                ? (ActionResult)new OkObjectResult(response)
                 : new BadRequestObjectResult("GIT: Please pass a name on the query string or in the request body");
         }
     }
