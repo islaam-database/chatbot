3f2f9d7aa3abb1a041aac6080cbc6f8dade4da05
Yousef Shanawany <askyous@gmail.com>
2019-06-14 19:24:12 -0700

    Searching a person works

--- fulfillment.cs
+++ fulfillment.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Google.Apis.Dialogflow.v2.Data;
+using islaam_db_client;
 
 namespace IslaamDatabase
 {
@@ -18,13 +19,25 @@ namespace IslaamDatabase
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            var data = JsonConvert.DeserializeObject<GoogleCloudDialogflowV2WebhookRequest>(requestBody);
-            var response = new GoogleCloudDialogflowV2WebhookResponse { FulfillmentText = "Hello yousef." };
+            var fulfillmentRequest = JsonConvert.DeserializeObject<GoogleCloudDialogflowV2WebhookRequest>(requestBody);
+            var idb = new IslaamDBClient(SECRETS.API_KEY);
+            string textResponse;
+            switch (fulfillmentRequest.QueryResult.Intent.DisplayName)
+            {
+                case "who-is":
+                    var query = fulfillmentRequest.QueryResult.Parameters["person"].ToString();
+                    var searchResult = idb.PersonAPI.Search(query);
+                    textResponse = searchResult[0].name;
+                    break;
+                default:
+                    textResponse = "Huh?";
+                    break;
+            }
 
-            return data != null
+            var response = new GoogleCloudDialogflowV2WebhookResponse { FulfillmentText = textResponse };
+
+            return fulfillmentRequest != null
                 ? (ActionResult)new OkObjectResult(response)
                 : new BadRequestObjectResult("GIT: Please pass a name on the query string or in the request body");
         }
