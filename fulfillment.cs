4f8217fcfd4482d70dc78cd45d5793ea8f40b2e3
Yousef Shanawany <askyous@gmail.com>
2019-08-24 15:50:43 -0700

    Smarter bio algorithm

--- fulfillment.cs
+++ fulfillment.cs
@@ -15,24 +15,56 @@ namespace IslaamDatabase
 {
     public static class Fulfillment
     {
+        public static int MAX_DIFF_FOR_SEARCH_RESULTS = 5;
+
         [FunctionName("fulfillment")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            string googleApiKey = req.Query["google-api-key"];
-            if (googleApiKey == null) return new BadRequestObjectResult("Missing 'google-api-key' as a parameter.");
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var googleApiKey = (string)req.Query["google-api-key"];
+            if (googleApiKey == null)
+                return new BadRequestObjectResult("Missing 'google-api-key' as a parameter.");
+
+            var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             var fulfillmentRequest = JsonConvert.DeserializeObject<GoogleCloudDialogflowV2WebhookRequest>(requestBody);
             var idb = new IslaamDBClient(googleApiKey);
             var responseObj = new List<string>() { };
-            switch (fulfillmentRequest.QueryResult.Intent.DisplayName)
+            var query = fulfillmentRequest.QueryResult.Parameters["person"].ToString();
+
+            var searchResults = idb.PersonAPI
+                .Search(query)
+                .FindAll(x => x.lavDistance <= MAX_DIFF_FOR_SEARCH_RESULTS)
+                .OrderBy(x => x.lavDistance)
+                .ToList();
+
+            // if no search results
+            if (searchResults.Count == 0)
             {
+                return new OkObjectResult(
+                    new GoogleCloudDialogflowV2WebhookResponse {
+                        FulfillmentText = $"Sorry. I couldn't find anyone named {query}.",
+                    }
+                );
+            }
+
+            var person = searchResults[0].person;
+            var intent = fulfillmentRequest.QueryResult.Intent.DisplayName;
+
+            switch (intent)
+            {
+                case "who-is":
+                    {
+                        responseObj.Add(person.BioIntro(idb));
+                        var responseAsText = string.Join(" ", responseObj);
+                        var response = new GoogleCloudDialogflowV2WebhookResponse
+                        {
+                            FulfillmentText = responseAsText,
+                        };
+                        return new OkObjectResult(response);
+                    }
                 case "get-teachers":
                     {
-                        var query = fulfillmentRequest.QueryResult.Parameters["person"].ToString();
-                        var searchResult = idb.PersonAPI.Search(query);
-                        var person = searchResult[0];
                         var teachers = idb.StudentsAPI
                             .GetData()
                             .Where(s => s.studentId == person.id)
@@ -42,22 +74,6 @@ namespace IslaamDatabase
                             ? $"{person.name}'s teachers include: \n\n {string.Join(", ", teachers)}"
                             : $"Sorry. I don't know of any teachers of {person.name} at the moment.";
                         return new OkObjectResult(new GoogleCloudDialogflowV2WebhookResponse { FulfillmentText = response });
-                    }
-                case "who-is":
-                    {
-                        var query = fulfillmentRequest.QueryResult.Parameters["person"].ToString();
-                        var searchResult = idb.PersonAPI.Search(query);
-                        var person = searchResult[0];
-
-                        // create response
-                        responseObj.Add($"{person.name}.");
-                        responseObj.Add(person.BioIntro(idb));
-                        var responseAsText = string.Join(" ", responseObj);
-                        var response = new GoogleCloudDialogflowV2WebhookResponse
-                        {
-                            FulfillmentText = responseAsText,
-                        };
-                        return new OkObjectResult(response);
                     }
             }
             return new OkObjectResult(new GoogleCloudDialogflowV2WebhookResponse { FulfillmentText = "Huh?" });
