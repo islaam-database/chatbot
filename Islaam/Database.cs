2bcf3b88820b5b9101380d14f3f1433cf2a864c6
Yousef Shanawany <askyous@gmail.com>
2019-09-08 08:39:46 -0700

    Added tables

--- Islaam/Database.cs
+++ Islaam/Database.cs
@@ -5,9 +5,14 @@ namespace Islaam
 {
     public class Database : DbContext
     {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Generation> Generations { get; set; }
         public DbSet<Person> People { get; set; }
-        public Database()
-        {
-        }
+        public DbSet<Praise> Praises { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<TeacherStudent> TeacherStudents { get; set; }
+        public DbSet<Title> Titles { get; set; }
+        public DbSet<Topic> Topics { get; set; }
     }
 }
